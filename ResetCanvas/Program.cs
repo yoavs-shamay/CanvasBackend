23856b5aa565c3658a1da2e9e3f32bbfec1277f7
yoav-shamay <yoav.s@bengurion.org>
2022-05-13 14:57:31 +0300

    changed database

--- ResetCanvas/Program.cs
+++ ResetCanvas/Program.cs
@@ -1,29 +1,30 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Threading.Tasks;
 
 namespace ResetCanvas
 {
     class Program
     {
         public const bool Reset = false;
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             if (Reset)
             {
-                var connectionString = "server=canvas-db.mysql.database.azure.com;userid=yodi555;password=1234qwer!@#$;database=Canvas;CharSet=utf8;";
+                var connectionString = "server=remotemysql.com;userid=xwcWQw3nCm;password=D6mQvJnLV5;database=xwcWQw3nCm;CharSet=utf8;";
                 int width = 100, height = 100;
                 var connection = new MySqlConnection(connectionString);
                 connection.Open();
-                var removeCanvasQuery = "TRUNCATE TABLE Pixels;";
+                var removeCanvasQuery = "TRUNCATE TABLE pixels;";
                 var removeCanvasCmd = new MySqlCommand(removeCanvasQuery, connection);
                 removeCanvasCmd.ExecuteNonQuery();
                 for (int i = 0; i < width; i++)
                 {
                     for (int j = 0; j < height; j++)
                     {
-                        var query = $"INSERT INTO Pixels(Red, Green, Blue, LastModifier, X, Y) VALUES(255, 255, 255, \"\", {i}, {j});";
+                        var query = $"INSERT INTO pixels(Red, Green, Blue, LastModifier, X, Y) VALUES(255, 255, 255, \"\", {i}, {j});";
                         var cmd = new MySqlCommand(query, connection);
-                        cmd.ExecuteNonQuery();
+                        await cmd.ExecuteNonQueryAsync();
                     }
                 }
             }
